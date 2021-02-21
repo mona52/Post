016767228c2078ad52dc447b6fee35137aa1a8cb
mona52 <mona52@list.ru>
2021-02-21 13:33:34 +0300

    add manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerID, String domain, String query, int ownersOnly, int count, int offset) {
+        // to do
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+        // to do
+    }
+
+}
